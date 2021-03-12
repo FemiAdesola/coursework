b8113e90ef601e717815e14f74a26d3100f7d222
FemiAdesola <moysem11@gmail.com>
2021-03-12 20:52:40 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
+
 // selection of set the state of menu bar true when it open and false when is close
 let showMenu = false;
 
@@ -17,6 +18,7 @@ function toggleMenu() {
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
+        
 
         // set menu state to be true when open
         showMenu = true;
@@ -26,6 +28,7 @@ function toggleMenu() {
         menuNav.classList.remove('show');
         menuBranding.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
+        
 
         // set menu state to be false when open clsoe
         showMenu = false;
